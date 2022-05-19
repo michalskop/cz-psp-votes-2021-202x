c0a6e3300a1579a2d201692dfb17502ef9c430c8
Michal Skop <michal.skop@kohovolit.eu>
2022-05-19 03:25:39 +0200

    govity

--- rebelity.py
+++ rebelity.py
@@ -18,6 +18,7 @@ data_path = "data/"
 
 current_term = 'PSP9'
 current_hlasovani = 2021
+gov_since = '2021-12-17'
 
 # download fresh data
 url = "http://www.psp.cz/eknih/cdrom/opendata/hl-2021ps.zip"
@@ -81,6 +82,7 @@ mps = pd.read_csv(path + data_path + "mps.csv")
 current_parliament_id = organy[(organy['type_org_id'] == parlament_id) & (organy['org_abbreviation'] == current_term)]['org_id'].iloc[0]
 
 valid_votes['org_id'] = pd.NA
+valid_votes['in_gov'] = False
 valid_votes = valid_votes.merge(vote_events.loc[:, ['vote_event_id', 'date']], on='vote_event_id')
 
 for index, row in mps.iterrows():
@@ -88,26 +90,39 @@ for index, row in mps.iterrows():
 
   t = zarazeni[zarazeni['id'] == mp['id']].merge(organy, left_on='of_id', right_on='org_id').merge(typ_organu, on='type_org_id')
   membership = t[(t['type_org_name_cs'] == 'Klub') & (t['sup_org_id'] == current_parliament_id)]
+  membershipgov = t[(t['type_org_name_cs'] == 'Vláda')]
 
-  mm = membership.iloc[0]
+  for i, mm in membership.iterrows():
+    valid_votes.loc[(valid_votes['date'] >= mm['since']) & ((valid_votes['date'] <= mm['until']) | (mm['until'] in ['nan', np.nan])) & (valid_votes['mp_id'] == mp['mp_id']), ['org_id']] = mm['org_id']
 
-  valid_votes.loc[(valid_votes['date'] >= mm['since']) & ((valid_votes['date'] <= mm['until']) | (mm['until'] in ['nan', np.nan])) & (valid_votes['mp_id'] == mp['mp_id']), ['org_id']] = mm['org_id']
+  for ii, mm in membershipgov.iterrows():
+    valid_votes.loc[(valid_votes['date'] >= mm['since']) & (valid_votes['date'] >= gov_since) & ((valid_votes['date'] <= mm['until']) | (mm['until'] in ['nan', np.nan])) & (valid_votes['mp_id'] == mp['mp_id']), ['in_gov']] = True
 
 # add vote_values (active)
 valid_votes['vote_value'] = pd.NA
 valid_votes['vote_value_active'] = pd.NA
+valid_votes['present'] = 0
 valid_votes.loc[valid_votes['vote'].isin(['A']), ['vote_value','vote_value_active']] = 1
 valid_votes.loc[valid_votes['vote'].isin(['B', 'N', 'K', 'C', 'F']), ['vote_value']] = -1
 valid_votes.loc[valid_votes['vote'].isin(['B', 'N']), ['vote_value_active']] = -1
 valid_votes.loc[valid_votes['vote'].isin(['@', 'W', 'M']), ['vote_value']] = 0
 valid_votes.loc[valid_votes['vote'].isin(['@', 'W', 'M', 'K', 'F', 'C']), ['vote_value_active']] = 0
+valid_votes.loc[valid_votes['vote'].isin(['A', 'N', 'K']), ['present']] = 1
 
 # add group vote and merge back
-pt = pd.pivot_table(valid_votes, index=['vote_event_id', 'org_id'], values=['vote_values'], aggfunc=np.sum).reset_index()
-pt['group_way'] = np.sign(pt['vote_values'])
-pt['group_way_abs'] = np.abs(np.sign(pt['vote_values']))
+pt = pd.pivot_table(valid_votes, index=['vote_event_id', 'org_id'], values=['vote_value'], aggfunc=np.sum).reset_index()
+pt['group_way'] = np.sign(pt['vote_value'])
+pt['group_way_abs'] = np.abs(np.sign(pt['vote_value']))
 
 valid_votes = valid_votes.merge(pt, on=['vote_event_id', 'org_id'])
+
+# add gov vote and merge back
+ptg = pd.pivot_table(valid_votes, index=['vote_event_id', 'in_gov'], values=['vote_value'], dropna=False, fill_value=0, aggfunc=np.sum).reset_index()
+ptgf = ptg[ptg['in_gov']]
+ptgf['gov_way'] = np.sign(ptgf['vote_value'])
+ptgf['gov_way_abs'] = np.abs(np.sign(ptgf['vote_value']))
+
+valid_votes = valid_votes.merge(ptgf.loc[:, ['vote_event_id', 'gov_way', 'gov_way_abs']], on='vote_event_id')
 
 # rebeling
 valid_votes['rebeling'] = 0
@@ -116,7 +131,16 @@ valid_votes.loc[valid_votes['vote_value_active'] * valid_votes['group_way'] == -
 rt = pd.pivot_table(valid_votes, index=['mp_id'], values=['rebeling', 'group_way_abs'], aggfunc=np.sum)
 rt['rebelity'] = rt['rebeling'] / rt['group_way_abs']
 
-# join with MPs
+# voting against government
+valid_votes['against_gov'] = 0
+valid_votes['possibly_against_gov'] = 0
+valid_votes.loc[valid_votes['vote_value_active'] * valid_votes['gov_way'] == -1, ['against_gov']] = 1
+valid_votes.loc[(valid_votes['present'] == 1) & (valid_votes['gov_way_abs'] == 1), ['possibly_against_gov']] = 1
+
+gt = pd.pivot_table(valid_votes, index=['mp_id'], values=['against_gov', 'possibly_against_gov'], aggfunc=np.sum)
+gt['govity'] = 1 - gt['against_gov'] / gt['possibly_against_gov']
+
+# join with MPs rebelity
 rebelity = mps[mps['in_parliament']].merge(rt, on=['mp_id'])
 
 rebelity.sort_values(by=['last_group_abbreviation', 'rebelity'], ascending=[True, False], inplace=True)
@@ -132,3 +156,22 @@ rebelity.rename(columns={'group_way_abs': 'possible'}, inplace=True)
 output = rebelity.loc[:, ['id', 'name', 'rebeling', 'possible', 'rebel', 'last_group_abbreviation', 'region_name_cs', 'photo_url']]
 
 output.to_csv(path + data_path + "rebelity.v1.csv", index=False)
+
+# join with MPs govity
+govity = mps[mps['in_parliament']].merge(gt, on=['mp_id'])
+
+govity.sort_values(by=['last_group_abbreviation', 'govity'], ascending=[True, False], inplace=True)
+
+# output v.1
+govity['photo_url'] = "https://www.psp.cz/eknih/cdrom/2021ps/eknih/2021ps/poslanci/i" + govity["id"].astype(str) + ".jpg" 
+govity['name'] = govity['given_name'] + " " + govity['family_name']
+
+govity['gover'] = round(1000 * govity['govity']) / 10
+
+govity.rename(columns={'possibly_against_gov': 'possible'}, inplace=True)
+
+output2 = govity.loc[:, ['id', 'name', 'against_gov', 'possible', 'gover', 'last_group_abbreviation', 'region_name_cs', 'photo_url']]
+
+output2.to_csv(path + data_path + "govity.v1.csv", index=False)
+
+
