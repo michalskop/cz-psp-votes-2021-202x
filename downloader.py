7f38831762a0d4a01f005a8c01face21549b604f
Michal Škop <michal.skop@seznam.cz>
2023-02-21 15:26:18 +0100

    Update downloader.py

--- downloader.py
+++ downloader.py
@@ -16,7 +16,7 @@ data_path = "source/"
 #     z.extractall(path + data_path)
 
 url = "https://www.psp.cz/eknih/cdrom/opendata/poslanci.zip"
-r = requests.get(url, verify=True)
+r = requests.get(url, verify=False)
 if r.ok:
   z = zipfile.ZipFile(io.BytesIO(r.content))
   z.extractall(path + data_path)
